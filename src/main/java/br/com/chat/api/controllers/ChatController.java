8f0c1d2d71b5055a65a8a60f1630f4c50bc9bdce
Nathan Fiorito <nfiorito64@gmail.com>
2024-10-07 22:08:03 -0300

    ...

--- src/main/java/br/com/chat/api/controllers/ChatController.java
+++ src/main/java/br/com/chat/api/controllers/ChatController.java
@@ -4,9 +4,12 @@ import br.com.chat.api.core.domain.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class ChatController {
+
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     public ChatMessage sendMessage(ChatMessage chatMessage) {
